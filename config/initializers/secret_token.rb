f35fd447180ca4b1b9b15b7590e31c22915b0b6f
Willis F. Jackson III <raidercat@gmail.com>
2012-05-02 13:58:49 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Getcoffee::Application.config.secret_token = 'ffd9bde5b72ea560d93c738d41d78847c886c65c7765ebecbb4f1f66a7e9dc18340d136f5b81c15c0600837fdbd407e1e78256455b92e88f35eb29c4f58a956f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Getcoffee::Application.config.session_store :cookie_store, key: '_getcoffee_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Getcoffee::Application.config.session_store :active_record_store
